d9e891b6c7830f9ba5c8c3130d8eadbdd460eeeb
지서경 <3skjee@naver.com>
2021-12-03 21:08:23 +0900

    added icon_all

--- src/help_board/index.js
+++ src/help_board/index.js
@@ -19,16 +19,14 @@ function IndexPage() {
     background: "#364d79",
   };
 
-  function IndexPage() {
-    const suffix = (
-      <AudioOutlined
-        style={{
-          fontSize: 16,
-          color: '#1890ff',
-        }}
-      />
-    );
-  }
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1890ff',
+      }}
+    />
+  );
 
   const { Meta } = Card;
 
@@ -48,6 +46,9 @@ function IndexPage() {
         {/* 도움 요청 게시판의 카테고리 */}
         <div id="categories">
             <div id="categories-icon">
+                <a href="#" className="icon">
+                    <img src="./images/icon/icon_all.png" alt="전체" />
+                </a>
                 <a href="#" className="icon">
                     <img src="./images/icon/icon_elderly.png" alt="노인" />
                 </a>
