b3f02c80bc7327e509b8ac3251f1f011e6f99ef8
지서경 <3skjee@naver.com>
2021-12-11 17:39:55 +0900

    iot_page_and_others_design_changed

--- src/01_Body/iot_apply/index.js
+++ src/01_Body/iot_apply/index.js
@@ -6,32 +6,29 @@ function IotAppplyPage() {
   return (
     <div>
       {/* <!-- container --> */}
-      <div className="container">        
-        <div id="iot-wrapper">          
-          <div id="iot-introduce">
-            <div id="iot-title">
-              <p>IOT 신청하기</p>
-            </div>
+      <div className="container">
+        <div id="iot-title">
+          <p>사용자 움직임 이상 감지 서비스</p>
+        </div>        
+        <div id="iot-wrapper">             
+          <div id="iot-introduce">            
             <div id="iot-introduce-left">
               <div id="iot-img">
                 <Image
-                  height={'400px'}
+                  width={'100%'}
                   src="./images/iot_img/iot_img.png"
                 />
               </div>
             </div>
             <div id="iot-introduce-right">
               <p id="iot-p">
-                iot 설명 1
+                당신의 움직임을 iot 센서가 감지하여<br />정각마다 1시간 동안의 움직임 정보를<br />인공지능 서버에 전달합니다.
               </p>
               <p id="iot-p">
-                iot 설명 2
+                인공지능 서버에서는 받아온 정보를 토대로<br />이상 탐지를 시행합니다.
               </p>
               <p id="iot-p">
-                iot 설명 3
-              </p>
-              <p id="iot-p">
-                iot 설명 4
+                움직임 정보에 이상이 탐지되면<br />iot에서 사이렌이 울립니다.
               </p>
             </div>
           </div>
@@ -39,7 +36,7 @@ function IotAppplyPage() {
             <Button
               //onClick={}
               size="large"
-              type="default"
+              type="primary"
             >
               <span>신청하기</span>
             </Button>
